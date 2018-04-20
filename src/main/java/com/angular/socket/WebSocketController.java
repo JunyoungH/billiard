5660a43948a8f7bbdd7f73c77ac1da7b7fcebf00
kensiro5218 <kensiro5218@gmail.com>
2018-04-20 23:36:36 +0800

    socketjs spring boot

---
+++ src/main/java/com/angular/socket/WebSocketController.java
@@ -0,0 +1,35 @@
+package com.angular.socket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+public class WebSocketController {
+
+    private static Logger logger = Logger.getLogger("WebSocketController.class");
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/message")
+    @SendTo("/topic/reply")
+    public String processMessageFromClient(@Payload String message) throws Exception{
+
+        logger.info(message);
+//        String data = new Gson().fromJson(message, Map.class).get("table").toString();
+        return message;
+    }
+
+    @MessageExceptionHandler
+    public String handleException(Throwable exception){
+        messagingTemplate.convertAndSend("/errors", exception.getMessage());
+        return exception.getMessage();
+    }
+}
